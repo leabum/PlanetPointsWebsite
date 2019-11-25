d36c400e22336eb0375c4fff787a45ac4c4280f7
Leander Burgmer <leabum@web.de>
2019-11-26 00:08:11 +0100

    Updates

---
+++ precache-manifest.7e97011fbcd4fb602ecaf1abbe561a87.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80c04456490bd90bd2fd88612fd25feb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9687ef0e4d15bb775c2",
+    "url": "/static/css/main.82a7cbf1.chunk.css"
+  },
+  {
+    "revision": "01cebece1d281c8ff2bc",
+    "url": "/static/js/2.c92abf84.chunk.js"
+  },
+  {
+    "revision": "c9687ef0e4d15bb775c2",
+    "url": "/static/js/main.c54870ec.chunk.js"
+  },
+  {
+    "revision": "ed4750f41f4543566353",
+    "url": "/static/js/runtime-main.fbe98da3.js"
+  },
+  {
+    "revision": "e760ce4999506944c72c3acbee4176dd",
+    "url": "/static/media/android-logo.e760ce49.png"
+  },
+  {
+    "revision": "5303f8d23dc3836e1828e72e29cde7f1",
+    "url": "/static/media/apple-logo.5303f8d2.png"
+  },
+  {
+    "revision": "ea2d0e2ef300fdfc1b27861da281abde",
+    "url": "/static/media/baby.ea2d0e2e.jpg"
+  },
+  {
+    "revision": "0547f374d5df6387a1f7da1e82d12e8d",
+    "url": "/static/media/background.0547f374.jpg"
+  },
+  {
+    "revision": "4924e7b140db0cbd64d4b18c7fca046e",
+    "url": "/static/media/bottle2.4924e7b1.jpg"
+  },
+  {
+    "revision": "f686265e7d5d1b71f37dde525a0be228",
+    "url": "/static/media/ceo_state1.f686265e.png"
+  },
+  {
+    "revision": "18bae035b9f7b841b9e558d175e974ce",
+    "url": "/static/media/contact_bg.18bae035.png"
+  },
+  {
+    "revision": "043af843281bb855b8a87304d0602b03",
+    "url": "/static/media/handy2.043af843.jpg"
+  },
+  {
+    "revision": "842e1869f9fe6176bc41f282958c212b",
+    "url": "/static/media/leander.842e1869.jpg"
+  },
+  {
+    "revision": "5196f5e52b95bd3cf362f9b5ac8a9856",
+    "url": "/static/media/logo-round.5196f5e5.png"
+  },
+  {
+    "revision": "7cb0c1d268d29765120fda0629b86a08",
+    "url": "/static/media/maurice.7cb0c1d2.jpg"
+  }
+]);
